af9cea6f8a4584e4851a793539c36abc4f4c877f
PaulBodin <pbodin@et.esiea.fr>
2021-12-06 10:54:35 +0100

    Partie 2 - Serveur HTTP

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -1,7 +1,17 @@
 package fr.lernejo.navy_battle;
 
-public class Launcher {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+import com.sun.net.httpserver.HttpServer;
+
+public class Launcher {
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(9876), 0);
+        ExecutorService executor = Executors.newFixedThreadPool(1);
+        server.createContext("/ping", new CallHandler());
+        server.start();
     }
 }
