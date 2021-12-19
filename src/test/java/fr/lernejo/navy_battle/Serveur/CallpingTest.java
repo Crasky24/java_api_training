7f4e21ce1fd98c89a91cf91fd3f97d85c9504b0c
PaulBodin <pbodin@et.esiea.fr>
2021-12-19 14:59:21 +0100

    Ajout Test CallpingTest

---
+++ src/test/java/fr/lernejo/navy_battle/Serveur/CallpingTest.java
@@ -0,0 +1,39 @@
+package fr.lernejo.navy_battle.Serveur;
+
+import com.sun.net.httpserver.HttpServer;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;;
+import java.net.http.HttpResponse;
+import java.util.concurrent.Executors;
+
+public class CallpingTest {
+
+    @Test
+    public void handle() throws IOException, InterruptedException {
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(9876), 0);
+        server.createContext("/ping", new Callping());
+
+        server.setExecutor(Executors.newFixedThreadPool(1));
+        server.start();
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create("http://localhost:9876/ping"))
+            .build();
+
+        final HttpResponse<?> response = client.send(request,
+            HttpResponse.BodyHandlers.ofString());
+
+        Assertions.assertThat(response.statusCode()).isEqualTo(200);
+        server.stop(1);
+
+    }
+}
